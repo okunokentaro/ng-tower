e6acdc3ce1eef1f7d08943f2e686f59ad6903757
OKUNOKENTARO <okunokentaro@me.com>
2016-07-14 21:57:23 +0900

    carve out to app.component

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'tw-app',
+  template: `
+    <div>ng-tower</div>
+  `,
+  directives: []
+})
+export class AppComponent {
+}
--- app/index.ts
+++ app/index.ts
@@ -3,25 +3,7 @@ import 'rxjs/Rx';
 import 'zone.js/dist/zone';
 
 import {bootstrap} from '@angular/platform-browser-dynamic';
-import {Component} from '@angular/core';
 
-@Component({
-  selector: 'hello-world',
-  template: `
-    <h1>Hello World!</h1>
-  `
-})
-class HelloWorldComponent {
-}
+import {AppComponent} from './app.component';
 
-@Component({
-  selector: 'my-app',
-  template: `
-    <hello-world></hello-world>
-  `,
-  directives: [HelloWorldComponent]
-})
-class MyAppComponent {
-}
-
-bootstrap(MyAppComponent);
+bootstrap(AppComponent);
