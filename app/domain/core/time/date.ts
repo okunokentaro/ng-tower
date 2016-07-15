bb8e189c15d003070b4939112020ed76fe2d2d4c
OKUNOKENTARO <okunokentaro@me.com>
2016-07-16 00:01:39 +0900

    implement date domain

---
+++ app/domain/core/time/date.ts
@@ -0,0 +1,21 @@
+import {AnyVal} from '../abstract/any-val';
+import {Frame} from '../frame/frame';
+import {Day} from './day';
+import {Hour} from './hour';
+import {Minute} from './minute';
+
+export class Date extends AnyVal<[Day, Hour, Minute]> {
+
+  static byFrame(f: Frame) {
+    return [
+      Day.byFrame(f),
+      Hour.byFrame(f),
+      Minute.byFrame(f)
+    ];
+  }
+
+  toString(): string {
+    return `${[this.value[0], this.value[1], this.value[2]]}`;
+  }
+
+}
