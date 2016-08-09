337a5835bf3317ebae68d145cdd9df754609ae71
OKUNOKENTARO <okunokentaro@me.com>
2016-08-09 23:23:29 +0900

    support walts 0.9.0-beta.1

--- app/actions/update-clock.action.ts
+++ app/actions/update-clock.action.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {Action, Reducer} from 'walts';
+import {Action, Next} from 'walts';
 
 import {AppState} from '../app.store';
 import {Date} from '../domain/core/time/date';
@@ -11,12 +11,10 @@ export class UpdateClockAction extends Action<AppState> {
     super();
   }
 
-  create(date: Date): Reducer<AppState> {
-    return (state: AppState) => {
-      let next = {} as AppState;
-      next.clock = date.clock();
-      return Promise.resolve(this.merge(state, next));
-    };
+  create(date: Date): Next<AppState> {
+    return (state: AppState) => ({
+      clock: date.clock()
+    } as AppState);
   }
 
 }
--- app/actions/update-parts-of-day.action.ts
+++ app/actions/update-parts-of-day.action.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {Action, Reducer} from 'walts';
+import {Action, Next} from 'walts';
 
 import {AppState} from '../app.store';
 import {Date} from '../domain/core/time/date';
@@ -11,12 +11,10 @@ export class UpdatePartsOfDayAction extends Action<AppState> {
     super();
   }
 
-  create(date: Date): Reducer<AppState> {
-    return (state: AppState) => {
-      let next = {} as AppState;
-      next.partsOfDay = date.currentPartsOfDay();
-      return Promise.resolve(this.merge(state, next));
-    };
+  create(date: Date): Next<AppState> {
+    return (state: AppState) => ({
+      partsOfDay: date.currentPartsOfDay()
+    } as AppState);
   }
 
 }
